9194cc499f9b488636c6c465d20e4604d08ab3a9
nwilson314 <nsw.wilson@gmail.com>
2024-11-30 08:43:17 -0500

    hmh - day 2 finished - SDL window and color change on resize

---
+++ handmade-hero/README.md
@@ -0,0 +1,10 @@
+# Handmade hero
+Handmade hero is a game engine written in C++ that uses the [SDL2](https://www.libsdl.org/) library. It follows the original [Handmade Hero tutorial](https://www.youtube.com/playlist?list=PLnuhp3Xd9PYTt6svyQPyRO_AAuMWGxPzU) with help from [Handmade Penguin](https://davidgow.net/handmadepenguin/default.html) (which, while incomplete, is also really good) for SDL2 conversion from the original Win32.
+
+I've included it in this specific repository for a couple of reasons:
+- I wanted to get better at reading and writing C++.
+- I've heard that when this series released, many of the programmers I admire pointed to it changing their career.
+- A full game engine in C++ will force me to understand many of the topics that I am wanting to cover in the other, more formal "courses."
+- This specific tutorial, following episodic, 1 hour tutorial videos is perfect for a "course" and honestly aligns fairly well with an undergraduate Game Engine course.
+
+As I am following along with the tutoral almost exactly, there isn't much code that I am creating from scratch. 
--- handmade-hero/code/sdl_handmade.cpp
+++ handmade-hero/code/sdl_handmade.cpp
@@ -1,13 +1,101 @@
 #include <SDL.h>
 
+bool HandleEvent(SDL_Event *Event)
+{
+    bool ShouldQuit = false;
+
+    switch (Event->type)
+    {
+        case SDL_QUIT:
+        {
+            printf("SDL_QUIT\n");
+            ShouldQuit = true;
+        } break;
+
+        case SDL_WINDOWEVENT:
+        {
+            switch(Event->window.event)
+            {
+                case SDL_WINDOWEVENT_RESIZED:
+                {
+                    printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", Event->window.data1, Event->window.data2);
+                } break;
+
+                case SDL_WINDOWEVENT_FOCUS_GAINED:
+                {
+                    printf("SDL_WINDOWEVENT_FOCUS_GAINED\n");
+                } break;
+
+                case SDL_WINDOWEVENT_EXPOSED:
+                {
+                    SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
+                    SDL_Renderer *Renderer = SDL_GetRenderer(Window);
+                    static bool IsWhite = true;
+                    if (IsWhite == true)
+                    {
+                        SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
+                        IsWhite = false;
+                    }
+                    else
+                    {
+                        SDL_SetRenderDrawColor(Renderer, 0, 0, 0, 255);
+                        IsWhite = true;
+                    }
+                    SDL_RenderClear(Renderer);
+                    SDL_RenderPresent(Renderer);
+                } break;
+            }
+        } break;
+    }
+    return(ShouldQuit);
+}
+
 int main(int argc, char *argv[])
 {
-    SDL_ShowSimpleMessageBox(
-        SDL_MESSAGEBOX_INFORMATION,
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        // TODO: SDL_Init didn't work!
+    }
+
+    SDL_Window *Window = SDL_CreateWindow(
         "Handmade Hero",
-        "This is Handmade Hero",
-        0
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        SDL_WINDOW_RESIZABLE
     );
+
+    if (Window)
+    {
+        SDL_Renderer *Renderer = SDL_CreateRenderer(
+            Window,
+            -1,
+            0
+        );
+        if (Renderer) 
+        {
+            for(;;)
+            {
+                SDL_Event Event;
+                SDL_WaitEvent(&Event);
+                if (HandleEvent(&Event))
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // TODO: Logging Renderer failed
+        }
+        
+    }
+    else
+    {
+        // TODO: Logging Window failed
+    }
     
+    SDL_Quit();
     return(0);
 }
